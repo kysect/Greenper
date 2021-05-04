6d3e4b9eda594f3ebd8034b35d892d6bf4b63fde
Annchous <annchooousss@gmail.com>
2021-05-04 23:00:11 +0300

    feat: base google sheets api accessor

---
+++ Greenper.Adapters.GoogleSheets/IGoogleSheetsApiAccessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Greenper.Adapters.GoogleSheets.GoogleSheetsResponses;
+
+namespace Greenper.Adapters.GoogleSheets
+{
+    public interface IGoogleSheetsApiAccessor
+    {
+        Task<SheetResponse> GetSheetAsync(String sheetId, String range);
+        Task<SpreadsheetResponse> GetSpreadsheetAsync(String spreadsheetId);
+    }
+}
