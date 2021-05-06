b106a2d9a1f3304d0559913bd1dd71a2354d833a
Annchous <annchooousss@gmail.com>
2021-05-06 03:07:51 +0300

    feat: sheets service config and spreadsheet api response

---
+++ Greenper.Adapters.GoogleSheets/Extensions/GoogleSheetsAdapterExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Apis.Sheets.v4.Data;
+using Greenper.Adapters.GoogleSheets.Models;
+
+namespace Greenper.Adapters.GoogleSheets.Extensions
+{
+    public static class GoogleSheetsAdapterExtensions
+    {
+        public static IEnumerable<GoogleSheetInfo> SelectSheets(this Spreadsheet spreadsheet) =>
+            spreadsheet.Sheets
+                .Select(sheet => sheet.Properties)
+                .Select(sheetProperty => new GoogleSheetInfo(sheetProperty.SheetId, sheetProperty.Title));
+    }
+}
