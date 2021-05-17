a368d5f73d65e0dc0f3742464563422616f62c47
Annchous <annchooousss@gmail.com>
2021-05-17 20:32:38 +0300

    feat: mapping

---
+++ Greenper.Core/Extensions/ColumnMappingExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Greenper.Core.Extensions
+{
+    public static class ColumnMappingExtensions
+    {
+        public static Int32 IndexOfColumn(this String column)
+        {
+            var rawColumn = new String(column
+                .TakeWhile(Char.IsLetter)
+                .ToArray());
+
+            var result = 0;
+            for (var i = rawColumn.Length - 1; i >= 0; i--)
+                result += (Int32)Math.Pow(26, i) * (rawColumn[i] - 'A' + 1);
+
+            return result;
+        }
+
+        public static Int32 FirstColumnIndex(this String range)
+        {
+            var firstColumn = new String(range
+                .SkipWhile(c => c != '!')
+                .Skip(1)
+                .TakeWhile(c => c != ':')
+                .ToArray());
+
+            return firstColumn.IndexOfColumn();
+        }
+    }
+}
---
+++ Greenper.Core/Mapping/MappingResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Greenper.Core.Mapping
+{
+    public class MappingResult<T>
+    {
+        public IEnumerable<T> MappedModels { get; set; }
+
+        public MappingResult(IEnumerable<T> mappedModels)
+        {
+            MappedModels = mappedModels;
+        }
+    }
+}
