a682f9bb8159a89074aa99db2979f732fc93e2ab
Annchous <annchooousss@gmail.com>
2021-07-05 22:31:54 +0300

    feat: column index

---
+++ Greenper.Core/Models/ColumnIndex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Greenper.Core.Models
+{
+    public class ColumnIndex
+    {
+        public String RawColumn { get; }
+
+        public ColumnIndex(String rawColumn)
+        {
+            RawColumn = rawColumn;
+        }
+
+        public String ColumnName => new String(RawColumn.TakeWhile(Char.IsLetter).ToArray());
+    }
+}
