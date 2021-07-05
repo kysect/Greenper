059a3f54f816c69431afa9ee8a8646f61e2880be
Annchous <annchooousss@gmail.com>
2021-07-05 22:31:06 +0300

    feat: index range and index extensions

---
+++ Greenper.Core/Models/IndexRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Greenper.Core.Extensions;
+
+namespace Greenper.Core.Models
+{
+    public class IndexRange
+    {
+        public String RawRange { get; }
+
+        private const Char SheetRangeDelimiter = '!';
+        private const Char ColumnRangeDelimiter = ':';
+
+        public IndexRange(String rawRange)
+        {
+            RawRange = rawRange;
+        }
+
+        public String SheetName => RawRange.TakeBefore(SheetRangeDelimiter);
+        public String ColumnIndexRange => RawRange.TakeAfter(SheetRangeDelimiter);
+        public String FirstColumnName => ColumnIndexRange.TakeBefore(ColumnRangeDelimiter, Char.IsLetter);
+        public String LastColumnName => ColumnIndexRange.TakeAfter(ColumnRangeDelimiter, Char.IsLetter);
+    }
+}
