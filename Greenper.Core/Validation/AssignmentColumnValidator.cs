ffd9dcbb2cbafa3165f18d219232c0e29634a3d9
Annchous <annchooousss@gmail.com>
2021-07-17 13:08:17 +0300

    refactor: move attributes to core project

--- Greenper.Core/Validation/AssignmentColumnValidator.cs
+++ Greenper.Core/Validation/AssignmentColumnValidator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Greenper.Attributes;
+using Greenper.Core.Attributes;
 using Greenper.Core.Extensions;
 
 namespace Greenper.Core.Validation
