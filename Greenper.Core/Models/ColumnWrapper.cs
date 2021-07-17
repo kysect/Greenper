f54f0f88c5a00c1288c56a90b429eaa98db6c4f3
Annchous <annchooousss@gmail.com>
2021-07-18 02:19:22 +0300

    refactor: encapsulate conversion logic

--- Greenper.Core/Models/ColumnWrapper.cs
+++ Greenper.Core/Models/ColumnWrapper.cs
@@ -6,7 +6,7 @@ namespace Greenper.Core.Models
     public class ColumnWrapper
     {
         private readonly String _columnName;
-        private readonly Int32? _columnNumber;
+        private readonly Int32 _columnNumber;
 
         public ColumnWrapper(String columnName)
         {
@@ -18,7 +18,31 @@ namespace Greenper.Core.Models
             _columnNumber = columnNumber;
         }
 
-        public Int32 ColumnNumber => _columnName?.IndexOfColumn() ?? throw new ArgumentNullException();
-        public String ColumnName => _columnNumber?.NameOfColumn() ?? throw new ArgumentNullException();
+        public Int32 ColumnNumber => GetColumnNumberFromName();
+        public String ColumnName => GetColumnNameFromNumber();
+
+        private Int32 GetColumnNumberFromName()
+        {
+            var result = 0;
+            for (var i = _columnName.Length - 1; i >= 0; i--)
+                result += (Int32)Math.Pow(26, i) * (_columnName[i] - 'A' + 1);
+
+            return result;
+        }
+
+        private String GetColumnNameFromNumber()
+        {
+            Int32 dividend = _columnNumber;
+            String result = String.Empty;
+
+            while (dividend > 0)
+            {
+                Int32 modulo = (dividend - 1) % 26;
+                result += Convert.ToChar('A' + modulo);
+                dividend = (dividend - modulo) / 26;
+            }
+
+            return result;
+        }
     }
 }
